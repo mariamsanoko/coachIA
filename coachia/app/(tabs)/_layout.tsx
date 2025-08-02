91b9f6c2994bf24e75ca72dcf9c22108b8149315
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-02 20:34:46 +0200

    Update _layout.tsx

--- coachia/app/(tabs)/_layout.tsx
+++ coachia/app/(tabs)/_layout.tsx
@@ -1,19 +1,47 @@
+import { Ionicons } from "@expo/vector-icons";
 import { Tabs } from "expo-router";
+
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs  
+      screenOptions={{
+        tabBarActiveTintColor: "#ffd33d",
+        headerStyle: {
+          backgroundColor: "#25292e",
+        },
+        headerShadowVisible: true,
+        headerTintColor: "#fff",
+        tabBarStyle: {
+          backgroundColor: "#25292e",
+        },
+      }}
+    >
       <Tabs.Screen 
         name="index" 
           options={{
             headerTitle: "Coach IA",
-            headerLeft: () => <></>
+            tabBarIcon: ({focused, color }) => (
+              <Ionicons 
+                name={focused ? "home-sharp" : "home-outline" }
+                color={color}
+               size={24} 
+              /> 
+            ),    
       }} 
     />
       <Tabs.Screen 
         name="about" 
         options={{
-          headerTitle: "À propos",
+          title: "À propos",
+          tabBarIcon: ({focused, color }) => (
+             <Ionicons 
+                name={focused ? "information-circle" : "information-circle" }
+                color={color}
+               size={24} 
+              /> 
+
+          ),
         }}
     />
       <Tabs.Screen 
