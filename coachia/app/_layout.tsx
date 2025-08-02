635886ce5cbbfe2b2333a400ef7261a701dacdb5
mariamsanoko <mariam.sanoko@gmail.com>
2025-08-02 20:59:02 +0200

    Update _layout.tsx

--- coachia/app/_layout.tsx
+++ coachia/app/_layout.tsx
@@ -1,18 +1,22 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { LogBox } from "react-native";
 
 LogBox.ignoreAllLogs(true);
 
 export default function RootLayout() {
   return (
+    <>
+    <StatusBar style="light" />
     <Stack>
       <Stack.Screen 
         name="(tabs)" 
           options={{
-           headerShown: false,
-      }} 
-    />
+           headerShown: false, 
+          }} 
+        />
       <Stack.Screen name="+not-found" options={{}} />
-      </Stack> 
+    </Stack>
+    </> 
   );
 }
